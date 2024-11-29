92285acd48bffe14700b0aabf612ad93780bc717
coraureum <aliquaemagica@tutanota.de>
2024-11-29 00:09:39 -0300

    CMake gen

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include "pHcontroller.cpp"
+#include "pHcalibration.cpp"
+#include <CppLinuxSerial/SerialPort.hpp>
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <stdexcept>
+
+int main() {
+	std::cout << "Hello World" << '\n';
+	return 0;
+}
+
